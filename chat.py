88b95df9b9638dfd8f5f923ab3f3e82cb4f7c8ca
Yousra jnahi <jyousra48@gmail.com>
2024-06-19 17:11:10 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -214,11 +214,11 @@ if 'initialized' not in st.session_state or st.session_state.selected_chain_type
     
     # Setup Pinecone
     pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
-    index_name = "docs-rag-summerday-modifie"
+    index_name = "docs-rag-summerday"
     if index_name not in pc.list_indexes().names():
         pc.create_index(name=index_name, dimension=384, metric="cosine", spec=ServerlessSpec(cloud="aws", region="us-east-1"))
 
-    namespace = "summerday-space-modifie"
+    namespace = "summerday-space"
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     db = PineconeVectorStore(index_name=index_name, embedding=embeddings, namespace=namespace)
 
