904f789f61648ac655902500563672d818827070
Yousra jnahi <jyousra48@gmail.com>
2024-06-28 22:59:26 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -111,47 +111,6 @@ loader_kwargs_map = {
 
 
 
-# Before calling the API, replace filename and ensure sdk is installed: "pip install unstructured-client"
-# See https://docs.unstructured.io/api-reference/api-services/sdk for more details
-
-import unstructured_client
-from unstructured_client.models import operations, shared
-
-client = unstructured_client.UnstructuredClient(
-    api_key_auth="VdJ2VXMxp8iV2a6Qtuga2xJJyekdI1",
-    server_url="https://api.unstructuredapp.io",
-)
-
-filename = "PATH_TO_FILE"
-with open(filename, "rb") as f:
-    data = f.read()
-
-req = operations.PartitionRequest(
-    partition_parameters=shared.PartitionParameters(
-        files=shared.Files(
-            content=data,
-            file_name=filename,
-        ),
-        # --- Other partition parameters ---
-        # Note: Defining 'strategy', 'chunking_strategy', and 'output_format'
-        # parameters as strings is accepted, but will not pass strict type checking. It is
-        # advised to use the defined enum classes as shown below.
-        strategy=shared.Strategy.AUTO,  
-        languages=['eng'],
-    ),
-)
-
-try:
-    res = client.general.partition(request=req)
-    print(res.elements[0])
-except Exception as e:
-    print(e)
-
-
-
-
-
-
 #############
 
 # Directory where you want to save the files
