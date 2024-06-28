14f181fc66ff76e4826ae7c1dd964d052d333fb1
Yousra jnahi <jyousra48@gmail.com>
2024-06-29 00:07:00 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -107,8 +107,7 @@ loader_cls_map = {
 # Define a mapping from file extensions to arguments loader
 loader_kwargs_map = {
                     
-            'pdf' : None,
-            'default': { 'strategy' :"fast"}
+            'default': None
 }
 
 
