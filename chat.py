abcda6df0d6d48d3a23a54eca6e1cf77c5aa5067
Yousra jnahi <jyousra48@gmail.com>
2024-06-28 09:39:08 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -26,8 +26,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter,Language
 uploaded_file = st.sidebar.file_uploader("Upload a document:",accept_multiple_files=True)  
 # Function to process uploaded file
 def process_uploaded_file(uploaded_file):
-  #upload_url= "/_stcore/upload_file/' + uploaded_file._file_urls
-  loader = TextLoader(uploaded_file[0])
+  upload_url= "/_stcore/upload_file/' + uploaded_file[0]._file_urls
+  loader = TextLoader(upload_url)
   documents = loader.load()
   text_splitter = RecursiveCharacterTextSplitter(chunk_size=300,chunk_overlap=100,length_function=len,add_start_index=True)
   chunks = text_splitter.split_documents(documents)
