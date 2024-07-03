54376670c58a21ed66acd0036f27900951fa9f6a
Yousra jnahi <jyousra48@gmail.com>
2024-07-03 10:55:58 +0200

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -2,15 +2,15 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
 import os 
+from src.embeddings import get_embeddings
 
 def get_vector_store(index_name, namespace):
   # Setup Pinecone
   pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
-  #index_name = "docs-rag-summerday"
   if index_name not in pc.list_indexes().names():
       pc.create_index(name=index_name, dimension=384, metric="cosine", spec=ServerlessSpec(cloud="aws", region="us-east-1"))
-  #namespace = "summerday-space"
-  embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+  #embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+  embeddings = get_embeddings("all-MiniLM-L6-v2")
   db = PineconeVectorStore(index_name=index_name, embedding=embeddings, namespace=namespace)
   index = pc.Index(name=index_name)
   return embeddings, db, index
