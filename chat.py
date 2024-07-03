5a3da637e537d0c56afd51188dd8f08648e5e25a
Yousra jnahi <jyousra48@gmail.com>
2024-07-03 16:10:21 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -66,7 +66,9 @@ spec=ServerlessSpec(cloud="aws", region="us-east-1")
 db, index = get_vector_store(index_name, namespace, embeddings, dimension, metric, spec)
 
 
-def page2():
+############# Display data ###################
+
+if st.sidebar.button("View Data"):
   embeddings, chunks, vectors, vector_ids =  get_data_in_vector_store(index,namespace)
   documents_projected = create_2d_embeddings(embeddings)
   df = vectordb_to_dfdb(documents_projected, chunks, vectors,vector_ids)
@@ -74,11 +76,7 @@ def page2():
   fig = df_visualisation(df)
   st.plotly_chart(fig)
 
-
-if st.button("Home"):
-  st.switch_page(page2)
-
-
+###############################################"
 
 
 def organize_files_by_extension(source_directory):
@@ -126,76 +124,11 @@ def split_documents(documents):
 
 
 
-# Function to create 2D embeddings
-def create_2d_embeddings(embeddings):
-    embedding_projector = pacmap.PaCMAP(n_components=2, n_neighbors=None, MN_ratio=0.5, FP_ratio=2.0, random_state=42)
-    embeddings_2d = embedding_projector.fit_transform(embeddings, init="pca")
-    return embeddings_2d
 
 
 
-if st.sidebar.button("View Data"):
-    embeddings, chunks, vectors, vector_ids =  get_data_in_vector_store(index,namespace)
-    documents_projected = create_2d_embeddings(embeddings)
-    
-    # Create DataFrame
-    df = pd.DataFrame.from_dict(
-        [
-            {
-                "x": documents_projected[i, 0],
-                "y": documents_projected[i, 1],
-                "source": chunks[i]["source"].split("/")[-1],
-                "extract": chunks[i]['text'][:100] + "...",
-                "symbol": "circle",
-                "size_col": 4,  # Reduced size
-                'vector': vectors[i],
-                'id': vector_ids[i]
-            }
-            for i in range(len(chunks))
-        ]
-    )
-    st.write(df)
 
-    # Generating a custom color map
-    num_unique_sources = df['source'].nunique()
-    colors = plt.get_cmap('tab20')(np.linspace(0, 1, num_unique_sources))
-    
-    color_discrete_map = {source: f'rgb({int(rgb[0]*255)}, {int(rgb[1]*255)}, {int(rgb[2]*255)})'
-                          for source, rgb in zip(df['source'].unique(), colors)}
-    
-    # Ensure user query has a distinct color
-    color_discrete_map["User query"] = "black"
-    
-    # Plot
-    fig = px.scatter(
-        df,
-        x="x",
-        y="y",
-        color="source",
-        hover_data="extract",
-        size="size_col",
-        symbol="symbol",
-        size_max=3,
-        color_discrete_map=color_discrete_map,
-        width=1000,
-        height=700
-    )
-    
-    fig.update_traces(
-        marker=dict(
-            opacity=0.7,
-            line=dict(width=0.5),
-            sizemode='diameter'
-        ),
-        selector=dict(mode="markers")
-    )
-    
-    fig.update_layout(
-        legend_title_text="<b>Chunk source</b>",
-        title="<b>2D Projection of Chunk Embeddings via PaCMAP</b>"
-    )
-    
-    st.plotly_chart(fig)
+
 
 ############################################ upload files ###################################################
 # File uploader in the sidebar
