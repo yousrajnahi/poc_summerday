c51d36b06311014620f8d23c0ee610d3567b2d89
Yousra jnahi <jyousra48@gmail.com>
2024-07-03 11:04:33 +0200

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -2,13 +2,13 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
 import os 
-from src.embeddings import get_embeddings
+
 
 def get_vector_store(index_name, namespace, embeddings, dimension, metric, spec):
   # Setup Pinecone
   pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
   if index_name not in pc.list_indexes().names():
-      pc.create_index(name=index_name, dimension=384, metric="cosine", spec=ServerlessSpec(cloud="aws", region="us-east-1"))
+      pc.create_index(name=index_name, dimension=dimension, metric=metric, spec=spec)
   db = PineconeVectorStore(index_name=index_name, embedding=embeddings, namespace=namespace)
   index = pc.Index(name=index_name)
   return db, index
