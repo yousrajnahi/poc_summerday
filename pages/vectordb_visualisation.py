b91351696c74faf46d72e9f661d0710df9385a01
Yousra jnahi <jyousra48@gmail.com>
2024-07-03 14:53:44 +0200

    Update vectordb_visualisation.py

--- pages/vectordb_visualisation.py
+++ pages/vectordb_visualisation.py
@@ -1,51 +1,10 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import streamlit as st
-import os
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
-from langchain.prompts.prompt import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from pinecone import Pinecone, ServerlessSpec
-from langchain.output_parsers.regex import RegexParser
-import warnings
-import subprocess
-import shutil
 import pacmap
 import plotly.express as px
 import numpy as np
 import pandas as pd
-
-
-from langchain_community.document_loaders import PDFMinerLoader, DirectoryLoader
-from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter,Language
 import os
-import tempfile
-from pdfminer.high_level import extract_text
-from langchain_community.document_loaders import (UnstructuredFileLoader,
-                                                  PDFMinerLoader,
-                                                  CSVLoader,
-                                                  JSONLoader,
-                                                  TextLoader,
-                                                  UnstructuredXMLLoader,
-                                                  UnstructuredHTMLLoader,
-                                                  UnstructuredMarkdownLoader,
-                                                  UnstructuredEmailLoader)
-
-
 import shutil
 import matplotlib.pyplot as plt
-
-import nltk
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
-
 from src.vector_store import get_vector_store, get_data_in_vector_store
 from src.embeddings import get_embeddings
 
@@ -55,72 +14,49 @@ def create_2d_embeddings(embeddings):
     embeddings_2d = embedding_projector.fit_transform(embeddings, init="pca")
     return embeddings_2d
 
+def vectordb_to_dfdb(documents_projected, chunks, vectors,vector_ids):
+  # Create DataFrame
+  df = pd.DataFrame.from_dict(
+    [
+        {
+            "x": documents_projected[i, 0],
+            "y": documents_projected[i, 1],
+            "source": chunks[i]["source"].split("/")[-1],
+            "extract": chunks[i]['text'][:100] + "...",
+            "symbol": "circle",
+            "size_col": 4,  # Reduced size
+            'vector': vectors[i],
+            'id': vector_ids[i]
+        }
+        for i in range(len(chunks))
+    ]
+  )
+  return df
+
+def df_visualisation(df):
+  # Generating a custom color map
+  num_unique_sources = df['source'].nunique()
+  colors = plt.get_cmap('tab20')(np.linspace(0, 1, num_unique_sources))
+  color_discrete_map = {source: f'rgb({int(rgb[0]*255)}, {int(rgb[1]*255)}, {int(rgb[2]*255)})'
+                      for source, rgb in zip(df['source'].unique(), colors)}
+  # Plot
+  fig = px.scatter(df, x="x", y="y", color="source", hover_data="extract", size="size_col", symbol="symbol", size_max=3, color_discrete_map=color_discrete_map, width=1000, height=700)
+  fig.update_traces(marker=dict(opacity=0.7,line=dict(width=0.5), sizemode='diameter'), selector=dict(mode="markers"))
+  fig.update_layout(legend_title_text="<b>Chunk source</b>", title="<b>2D Projection of Chunk Embeddings via PaCMAP</b>")
+  return fig
+
 index_name = "docs-rag-summerday"
 namespace = "summerday-space"
 embedding_model_name = "all-MiniLM-L6-v2"
 embeddings = get_embeddings(embedding_model_name)
-dimension=384 
-metric="cosine"
-spec=ServerlessSpec(cloud="aws", region="us-east-1")
+dimension = 384 
+metri c= "cosine"
+spec = ServerlessSpec(cloud="aws", region="us-east-1")
 db, index = get_vector_store(index_name, namespace, embeddings, dimension, metric, spec)
 embeddings, chunks, vectors, vector_ids =  get_data_in_vector_store(index,namespace)
 documents_projected = create_2d_embeddings(embeddings)
-
-# Create DataFrame
-df = pd.DataFrame.from_dict(
-  [
-      {
-          "x": documents_projected[i, 0],
-          "y": documents_projected[i, 1],
-          "source": chunks[i]["source"].split("/")[-1],
-          "extract": chunks[i]['text'][:100] + "...",
-          "symbol": "circle",
-          "size_col": 4,  # Reduced size
-          'vector': vectors[i],
-          'id': vector_ids[i]
-      }
-      for i in range(len(chunks))
-  ]
-)
+df = vectordb_to_dfdb(documents_projected, chunks, vectors,vector_ids)
 st.write(df)
-
-# Generating a custom color map
-num_unique_sources = df['source'].nunique()
-colors = plt.get_cmap('tab20')(np.linspace(0, 1, num_unique_sources))
-
-color_discrete_map = {source: f'rgb({int(rgb[0]*255)}, {int(rgb[1]*255)}, {int(rgb[2]*255)})'
-                    for source, rgb in zip(df['source'].unique(), colors)}
-
-# Ensure user query has a distinct color
-color_discrete_map["User query"] = "black"
-
-# Plot
-fig = px.scatter(
-  df,
-  x="x",
-  y="y",
-  color="source",
-  hover_data="extract",
-  size="size_col",
-  symbol="symbol",
-  size_max=3,
-  color_discrete_map=color_discrete_map,
-  width=1000,
-  height=700
-)
-
-fig.update_traces(
-  marker=dict(
-      opacity=0.7,
-      line=dict(width=0.5),
-      sizemode='diameter'
-  ),
-  selector=dict(mode="markers")
-)
-
-fig.update_layout(
-  legend_title_text="<b>Chunk source</b>",
-  title="<b>2D Projection of Chunk Embeddings via PaCMAP</b>"
-)
-
+fig = df_visualisation(df)
 st.plotly_chart(fig)
+
