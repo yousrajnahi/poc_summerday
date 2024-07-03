643447b75fc53f7f0548b14e04561e13f0c3fa15
Yousra jnahi <jyousra48@gmail.com>
2024-07-03 14:32:06 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -51,7 +51,9 @@ from src.embeddings import get_embeddings
 
 pages = {
     "Your account" : [
-        st.Page("vectordb_visualisation.py", title="Visulaisation"),
+      st.Page("vectordb_visualisation.py", title="Visulaisation", icon="🔥"),
+       st.Page("vectordb_visualisation.py", title="Visulaisation"), icon="🔥"),
+
     ]
     
 }
