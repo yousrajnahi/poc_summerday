73f6e40967ff8f77caeff64f65f0b358297ed90f
Yousra jnahi <jyousra48@gmail.com>
2024-07-04 09:31:51 +0200

    Update 01_Chat.py

--- pages/01_Chat.py
+++ pages/01_Chat.py
@@ -1,14 +1,7 @@
 import streamlit as st
 import os
-from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
-from langchain.prompts.prompt import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from langchain.output_parsers.regex import RegexParser
-from pinecone import Pinecone, ServerlessSpec
-from src.vector_store import get_vector_store
-from src.embeddings import get_embeddings
-from src.document_processing import read_template_from_file
+from src.utils import initialize_vector_store, get_or_create_retrieval_chain
+
 
 st.title("Chat")
 
@@ -24,60 +17,11 @@ model_options = ["gemma-7b-it", "mixtral-8x7b-32768", "llama3-70b-8192", "llama3
 default_model = "llama3-70b-8192"
 selected_model = st.sidebar.selectbox("Choose a model:", options=model_options, index=model_options.index(default_model))
 
-# Initialize vector store and embeddings
-index_name = "docs-rag-summerday"
-namespace = "summerday-space"
-embedding_model_name = "all-MiniLM-L6-v2"
-embeddings = get_embeddings(embedding_model_name)
-dimension = 384
-metric = "cosine"
-spec = ServerlessSpec(cloud="aws", region="us-east-1")
-db, index = get_vector_store(index_name, namespace, embeddings, dimension, metric, spec)
+# Initialize vector store
+db, index = initialize_vector_store()
 
-# Initialize retrieval chain
-if 'retrieval_chain' not in st.session_state or st.session_state.selected_chain_type != selected_chain_type or st.session_state.selected_model != selected_model:
-    st.session_state.selected_chain_type = selected_chain_type
-    st.session_state.selected_model = selected_model
-
-    # Load templates
-    templates = {
-        'map_reduce_question': read_template_from_file('prompt_templates/map_reduce_question_template.txt'),
-        'map_reduce_combine': read_template_from_file('prompt_templates/map_reduce_combine_template.txt'),
-        'map_rerank': read_template_from_file('prompt_templates/map_rerank_template.txt'),
-        'stuff': read_template_from_file('prompt_templates/stuff_template.txt'),
-        'refine': read_template_from_file('prompt_templates/refine_template.txt'),
-        'refine_question': read_template_from_file('prompt_templates/refine_question_template.txt')
-    }
-
-    # Define chain type kwargs
-    chain_type_kwargs = {
-        "map_reduce": {
-            "question_prompt": PromptTemplate(input_variables=["context", "question", "history"], template=templates['map_reduce_question']),
-            "combine_prompt": PromptTemplate(input_variables=["question", "summaries", "history"], template=templates['map_reduce_combine']),
-            "memory": ConversationBufferMemory(memory_key="history", input_key="question")
-        },
-        "map_rerank": {
-            "prompt": PromptTemplate(input_variables=["context", "question"], template=templates['map_rerank'], 
-                                     output_parser=RegexParser(regex=r"(.*?)\nScore: (\d*)", output_keys=["answer", "score"])),
-            "memory": ConversationBufferMemory(memory_key="history", input_key="question")
-        },
-        "refine": {
-            "refine_prompt": PromptTemplate(input_variables=["question", "existing_answer", "history", "context_str"], template=templates['refine']),
-            "question_prompt": PromptTemplate(input_variables=["history", "context_str", "question"], template=templates['refine_question']),
-            "memory": ConversationBufferMemory(memory_key="history", input_key="question")
-        },
-        "stuff": {
-            "prompt": PromptTemplate(input_variables=["history", "context", "question"], template=templates['stuff']),
-            "memory": ConversationBufferMemory(memory_key="history", input_key="question")
-        }
-    }   
-
-    # Setup Groq
-    llm = ChatGroq(temperature=0, model_name=selected_model)
-    
-    # Setup the retrieval_chain
-    args = chain_type_kwargs.get(selected_chain_type, chain_type_kwargs['stuff'])
-    st.session_state.retrieval_chain = RetrievalQA.from_chain_type(llm, chain_type=selected_chain_type, retriever=db.as_retriever(search_type='mmr'), chain_type_kwargs=args)
+# Get or create retrieval chain
+retrieval_chain = get_or_create_retrieval_chain(selected_chain_type, selected_model, db)
 
 # Chat interface
 if "messages" not in st.session_state:
@@ -96,7 +40,7 @@ if prompt := st.chat_input("What is up?"):
         if len(prompt.split()) > 500:
             st.warning(f"Your input is too long and will be truncated to fit the model's limit of 500 tokens.")
             prompt = ' '.join(prompt.split()[:500])
-        response = st.session_state.retrieval_chain.run(prompt)
+        response = retrieval_chain.run(prompt)
         matching_docs = db.as_retriever(search_type='mmr').get_relevant_documents(prompt)
         sources = [doc.metadata.get("source", doc.metadata) for doc in matching_docs]
         st.markdown(response)
@@ -109,3 +53,4 @@ if prompt := st.chat_input("What is up?"):
 # Sidebar Clear Chat Button
 if st.sidebar.button("Clear Chat"):
     st.session_state.messages.clear()
+
