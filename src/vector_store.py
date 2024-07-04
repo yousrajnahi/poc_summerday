b51cadc08ae6ccbedda647699cb924ac1b46478c
Yousra jnahi <jyousra48@gmail.com>
2024-07-04 09:47:33 +0200

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -11,7 +11,7 @@ def get_vector_store(index_name, namespace, embeddings, dimension, metric, spec)
   pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
   if index_name not in pc.list_indexes().names():
       pc.create_index(name=index_name, dimension=dimension, metric=metric, spec=spec)
-  db = PineconeVectorStore(index_name=index_name, embedding=embeddings, namespace=namespace)
+  db = PineconeVectorStore(index_name=index_name,embedding=embeddings,namespace=namespace)
   index = pc.Index(name=index_name)
   return db, index
 
