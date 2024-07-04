7ba20f269d2e04cda30e53aecb348b13c2669b95
Yousra jnahi <jyousra48@gmail.com>
2024-07-04 09:34:20 +0200

    Update 02_Data_Visualization.py

--- pages/02_Data_Visualization.py
+++ pages/02_Data_Visualization.py
@@ -1,24 +1,18 @@
 import streamlit as st
-from src.vector_store import get_vector_store, get_data_in_vector_store
-from src.embeddings import get_embeddings
-from src.displays import vectordb_to_dfdb, df_visualisation, create_2d_embeddings
-from pinecone import ServerlessSpec
+from src.utils import initialize_vector_store
+from src.vector_store import get_data_in_vector_store
+from src.embeddings import create_2d_embeddings
+from src.displays import vectordb_to_dfdb, df_visualisation
+
 
 st.title("Data Visualization")
 
-# Initialize vector store and embeddings
-index_name = "docs-rag-summerday"
-namespace = "summerday-space"
-embedding_model_name = "all-MiniLM-L6-v2"
-embeddings = get_embeddings(embedding_model_name)
-dimension = 384
-metric = "cosine"
-spec = ServerlessSpec(cloud="aws", region="us-east-1")
-db, index = get_vector_store(index_name, namespace, embeddings, dimension, metric, spec)
+# Initialize vector store
+db, index = initialize_vector_store()
 
 if st.button("View Data"):
     with st.spinner("Fetching and processing data..."):
-        embeddings, chunks, vectors, vector_ids = get_data_in_vector_store(index, namespace)
+        embeddings, chunks, vectors, vector_ids = get_data_in_vector_store(index, st.session_state.vector_store.namespace)
         documents_projected = create_2d_embeddings(embeddings)
         df = vectordb_to_dfdb(documents_projected, chunks, vectors, vector_ids)
         
@@ -29,3 +23,4 @@ if st.button("View Data"):
         fig = df_visualisation(df)
     
     st.plotly_chart(fig)
+
