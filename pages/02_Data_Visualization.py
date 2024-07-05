52164eed34a63c7cc83b72f59d0fafa2bd091e48
Yousra jnahi <jyousra48@gmail.com>
2024-07-05 17:58:08 +0200

    Update 02_Data_Visualization.py

--- pages/02_Data_Visualization.py
+++ pages/02_Data_Visualization.py
@@ -9,28 +9,30 @@ st.title("Data Visualization")
 # Initialize vector store
 db, index, namespace, embeddings = initialize_vector_store()
 
-if st.sidebar.button("View Data"):
-    with st.spinner("Fetching and processing data..."):
-        embeddings, chunks, vectors, vector_ids = get_data_in_vector_store(index, namespace)
-        
-        # Check if there's a last query to include
-        if 'last_query' in st.session_state and 'last_query_vector' in st.session_state:
-            embeddings = np.append(embeddings, [st.session_state['last_query_vector']], axis=0)
-            chunks.append({"source": "User query", "text": st.session_state['last_query']})
-            vectors.append(st.session_state['last_query_vector'])
-            vector_ids.append("id_user_query")
+if st.sidebar.button("View Data") or 'data_viz_df' in st.session_state:
+    if 'data_viz_df' not in st.session_state:
+        with st.spinner("Fetching and processing data..."):
+            embeddings, chunks, vectors, vector_ids = get_data_in_vector_store(index, namespace)
             
-        documents_projected = create_2d_embeddings(embeddings)
-        
-        # Use get() method to safely access 'matching_docs' from session state
-        matching_docs = st.session_state.get('matching_docs', None)
-        df = vectordb_to_dfdb(documents_projected, chunks, vectors, vector_ids, matching_docs)
-        
-        # Mark the user query point if it exists
-        if 'last_query' in st.session_state:
-            df.loc[df['id'] == 'id_user_query', 'symbol'] = 'star'
-            df.loc[df['id'] == 'id_user_query', 'size_col'] = 4  # Make user query point larger
-        
+            if 'last_query' in st.session_state and 'last_query_vector' in st.session_state:
+                embeddings = np.append(embeddings, [st.session_state['last_query_vector']], axis=0)
+                chunks.append({"source": "User query", "text": st.session_state['last_query']})
+                vectors.append(st.session_state['last_query_vector'])
+                vector_ids.append("id_user_query")
+                
+            documents_projected = create_2d_embeddings(embeddings)
+            
+            matching_docs = st.session_state.get('matching_docs', None)
+            df = vectordb_to_dfdb(documents_projected, chunks, vectors, vector_ids, matching_docs)
+            
+            if 'last_query' in st.session_state:
+                df.loc[df['id'] == 'id_user_query', 'symbol'] = 'star'
+                df.loc[df['id'] == 'id_user_query', 'size_col'] = 4
+            
+            st.session_state['data_viz_df'] = df
+    else:
+        df = st.session_state['data_viz_df']
+
     st.write("Data from Vector Store:")
     st.write(df)
     
@@ -39,6 +41,10 @@ if st.sidebar.button("View Data"):
     
     st.plotly_chart(fig)
 
-# Optionally, display the last query
+if st.sidebar.button("Clear Visualization"):
+    if 'data_viz_df' in st.session_state:
+        del st.session_state['data_viz_df']
+    st.rerun()
+
 if 'last_query' in st.session_state:
     st.sidebar.write(f"Last query: {st.session_state['last_query']}")
