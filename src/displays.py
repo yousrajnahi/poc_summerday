fafc81c5d489588464f8afbb9eba52f53d3b6738
Yousra jnahi <jyousra48@gmail.com>
2024-07-05 23:17:32 +0200

    Update displays.py

--- src/displays.py
+++ src/displays.py
@@ -6,7 +6,7 @@ import os
 import shutil
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import MinMaxScaler
 np.random.seed(42)
 
 
@@ -15,9 +15,9 @@ def create_2d_embeddings(embeddings):
     tsne = TSNE(n_components=2, random_state=42)
     embeddings_2d = tsne.fit_transform(embeddings)
     # Normalize the embeddings
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    embeddings_2d_normalized = scaler.fit_transform(embeddings_2d)
-    return embeddings_2d_normalized
+    #scaler = MinMaxScaler(feature_range=(-1, 1))
+    #embeddings_2d_normalized = scaler.fit_transform(embeddings_2d)
+    return embeddings_2d
 
 def vectordb_to_dfdb(documents_projected, chunks, vectors, vector_ids, matching_docs=None):
     source_matching_docs = []
@@ -47,15 +47,9 @@ def vectordb_to_dfdb(documents_projected, chunks, vectors, vector_ids, matching_
     return df
 
 def df_visualisation(df):
-  # Generating a custom color map
-  num_unique_sources = df['source'].nunique()
-  colors = plt.get_cmap('tab20')(np.linspace(0, 1, num_unique_sources))
-  color_discrete_map = {source: f'rgb({int(rgb[0]*255)}, {int(rgb[1]*255)}, {int(rgb[2]*255)})'
-                      for source, rgb in zip(df['source'].unique(), colors)}
-  # Ensure user query has a distinct color
-  color_discrete_map["User query"] = "black"
+ 
   # Plot
-  fig = px.scatter(df, x="x", y="y", color="source", size="size_col", symbol="symbol", size_max=3, color_discrete_map=color_discrete_map, width=1000, height=700)
+  fig = px.scatter(df, x="x", y="y", color="source", size="size_col", symbol="symbol", size_max=3, width=1000, height=700)
   fig.update_traces(marker=dict(opacity=0.7,line=dict(width=0.5), sizemode='diameter'), selector=dict(mode="markers"))
   fig.update_layout(legend_title_text="<b>Chunk source</b>", title="<b>2D Projection of Chunk Embeddings via PaCMAP</b>")
   return fig
